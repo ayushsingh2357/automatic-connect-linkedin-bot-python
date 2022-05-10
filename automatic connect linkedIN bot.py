9950e63063d11905ebefa8bf2c6af683a53ed64f
AYUSH <63778079+ayushsingh2357@users.noreply.github.com>
2022-05-10 11:08:22 +0530

    Add files via upload

---
+++ automatic connect linkedIN bot.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome("< your chrome driver location on drive>")
+driver.get(https://www.linkedin.com')
+time.sleep(2)
+
+
+username = driver.find_element_by_xpath("//input[@name='session_key']")
+password = driver.find_element_by_xpath("//input[@name='session_password']")
+
+username.send_keys('<your username>')
+password.send_keys('<your password> ')
+time.sleep(2)
+
+submit = driver.find_element_by_xpath("//button[@type='submit']").click()
+
+driver.get('<Enter your search url>')
+time.sleep(2)
+
+all_buttons = driver.find_elements_by_tag_name("button")
+connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
+
+for btn in connect_buttons:
+	driver.execute_script("arguments[0].click();", btn)
+	time.sleep(2)
+	send = driver.find_element_by_xpath("//button[@aria-label='Send now']")
+	driver.execute_script("arguments[0].click();", send)
+	close = drive.find_element_by_xpath("//button[@aria-label='Dismiss']")
+driver.execute_script("arguments[0].click();", close)
+	time.sleep(2)
+ 
